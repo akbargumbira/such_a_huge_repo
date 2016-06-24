2411f5896af68a7dc0beba631a548e3900c7b3e9
Akbar Gumbira <akbargumbira@gmail.com>
2016-06-24 21:50:05 +0200

    Fix general section.

--- generate.py
+++ generate.py
@@ -4,14 +4,14 @@ Config = ConfigParser.ConfigParser()
 
 cfgfile = open("metadata.ini", 'w')
 
-Config.add_section('General')
-Config.set('General', 'author', 'John Doe')
-Config.set('General', 'email', 'john_doe@microsoft.com')
+Config.add_section('general')
+Config.set('general', 'author', 'John Doe')
+Config.set('general', 'email', 'john_doe@microsoft.com')
 
 collections = []
 for i in range(1000):
     collections.append('collection_%s' % i)
-Config.set('General', 'collections', ','.join(collections))
+Config.set('general', 'collections', ','.join(collections))
 
 for i, collection in enumerate(collections):
     Config.add_section(collection)
